0444c4648bd8ee5dc223fabe58442a34c0829df2
李勇 <2236472089@qq.com>
2025-04-20 01:26:35 +0800

    feat: 新增 Screenfull 组件及其全屏切换功能

---
+++ package-mb/src/components/Screenfull/index.ts
@@ -0,0 +1,1 @@
+export { default as Screenfull } from './src/Screenfull.vue';
---
+++ package-mb/src/components/Screenfull/src/Screenfull.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useFullscreen } from '@vueuse/core';
+
+import { Icon } from '#/components/Icon';
+import { propTypes } from '#/utils/propTypes';
+
+defineProps({
+  color: propTypes.string.def(''),
+});
+
+const { isFullscreen, toggle } = useFullscreen();
+
+const toggleFullscreen = () => {
+  toggle();
+};
+</script>
+
+<template>
+  <div class="screenfull" @click="toggleFullscreen">
+    <Icon
+      :size="18"
+      :icon="isFullscreen ? 'zmdi:fullscreen-exit' : 'zmdi:fullscreen'"
+      :color="color"
+    />
+  </div>
+</template>
